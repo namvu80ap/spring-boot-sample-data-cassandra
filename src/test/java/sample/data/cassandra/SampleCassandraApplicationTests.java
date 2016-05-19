c2cb23d7dcff3195536006645dcd81e69e9cb049
Namvu <nam.vu@globalsign.com>
2016-05-19 13:19:44 +0900

    Initial commit

---
+++ src/test/java/sample/data/cassandra/SampleCassandraApplicationTests.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.data.cassandra;
+
+import org.cassandraunit.spring.CassandraDataSet;
+import org.cassandraunit.spring.EmbeddedCassandra;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.OutputCapture;
+
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestExecutionListeners.MergeMode;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ * Tests for {@link SampleCassandraApplication}.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners(mergeMode = MergeMode.MERGE_WITH_DEFAULTS, listeners = {
+		OrderedCassandraTestExecutionListener.class })
+@CassandraDataSet(keyspace = "mykeyspace", value = "setup.cql")
+@EmbeddedCassandra(timeout = 60000)
+public class SampleCassandraApplicationTests {
+
+	@ClassRule
+	public static OutputCapture outputCapture = new OutputCapture();
+
+	@Test
+	public void testDefaultSettings() throws Exception {
+		String output = SampleCassandraApplicationTests.outputCapture.toString();
+		//assertThat(output).contains("firstName='Alice', lastName='Smith'");
+	}
+
+}
